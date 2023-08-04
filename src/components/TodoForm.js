7503fb97def9e54917dbe9aade226d1b60cc949c
John Doe <betiaman@example.com>
2023-08-04 01:35:05 -0500

    todo app component working

---
+++ src/components/TodoForm.js
@@ -0,0 +1,25 @@
+// src/components/TodoForm.js
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/todoSlice';
+
+const TodoForm = () => {
+const [text, setText] = useState('');
+const dispatch = useDispatch();
+
+const handleSubmit = e => {
+    e.preventDefault();
+    if (text.trim() === '') return;
+    dispatch(addTodo({ id: Date.now(), text }));
+    setText('');
+};
+
+return (
+    <form onSubmit={handleSubmit}>
+    <input type="text" value={text} onChange={e => setText(e.target.value)} />
+    <button type="submit">Add</button>
+    </form>
+);
+};
+
+export default TodoForm;
---
+++ src/components/TodoList.js
@@ -0,0 +1,26 @@
+// src/components/TodoList.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeTodo } from '../redux/todoSlice';
+
+const TodoList = () => {
+const todos = useSelector(state => state.todos.todos);
+const dispatch = useDispatch();
+
+const handleRemoveTodo = id => {
+    dispatch(removeTodo(id));
+};
+
+return (
+    <ul>
+    {todos.map(todo => (
+        <li key={todo.id}>
+        {todo.text}
+        <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+        </li>
+    ))}
+    </ul>
+);
+};
+
+export default TodoList;
